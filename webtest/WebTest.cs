ca5cd0db6e786f497eb2b87528b03a348b4b7cb3
Arun Mohan <sreearunan@gmail.com>
2023-01-06 11:20:36 +0100

    first commit

---
+++ webtest/WebTest.cs
@@ -0,0 +1,30 @@
+using WebApplication1.Modules;
+
+namespace webtest
+{
+
+    public class WebTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            int i = 1;
+            bool result = false;
+            if (i == 1) result = true;
+            Assert.True(result, "Value should be equal to 1");
+
+        }
+        [Fact]
+        public void doAddCheck()
+        {
+            Functions func = new Functions();
+            int y = func.Add(3, 2);
+
+            bool result = false;
+            if (y == 5) result = true;
+            {
+                Assert.True(result, "Value should be equal to 5");
+            }
+        }
+    }
+}
